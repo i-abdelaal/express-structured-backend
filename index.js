5e83432dd0676107af12999377b57f1a6b8169a6
Ibrahim Shaker <shaker.personnel@gmail.com>
2020-09-10 05:37:22 +0200

    adding customers routes

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const express = require("express");
 const app = express();
-const Joi = require("joi");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const config = require("config");
 const startupDebugger = require("debug")("app:startup");
 const dbDebugger = require("debug")("app:db");
-const genres = require("./routes/genres");
+const mongoose = require("mongoose");
+
 const home = require("./routes/home");
+const genres = require("./routes/genres");
+const customers = require("./routes/customers");
+
+mongoose
+  .connect("mongodb://localhost/genres", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB/genres..."))
+  .catch((err) => console.error("Could not connect to genres ", err));
+mongoose
+  .connect("mongodb://localhost/customers", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB/customers..."))
+  .catch((err) => console.error("Could not connect to customers", err));
 
 app.set("view engine", "pug");
 app.set("views", "./views");
@@ -16,8 +33,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(helmet());
-app.use("/genres", genres);
 app.use("/", home);
+app.use("/genres", genres);
+app.use("/customers", customers);
 
 // console.log(`Application Name: ${config.get("name")}`);
 // console.log(`Mail Server: ${config.get("mail.host")}`);
--- routes/genres.js
+++ routes/genres.js
@@ -1,42 +1,82 @@
 const express = require("express");
+const Joi = require("joi");
+const mongoose = require("mongoose");
+
 const router = express.Router();
 
-const genres = [
-  { id: 1, genre: "Art" },
-  { id: 2, genre: "Science" },
-];
+const genreSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50,
+  },
+});
 
-const validate404 = (validatedItemId, res) => {
-  const item = genres.find((i) => i.id === parseInt(validatedItemId));
-  if (!item) return res.status(404).send(`${validatedItemId} is not found`);
-  return item;
-};
+const Genre = mongoose.model("Genre", genreSchema);
+
+// const validate404 = (validatedItemId, res) => {
+//   const item = genres.find((i) => i.id === parseInt(validatedItemId));
+//   if (!item) return res.status(404).send(`${validatedItemId} is not found`);
+//   return item;
+// };
 
 const validate400 = (validatedItemBody, res) => {
   const schema = Joi.object({
-    genre: Joi.string().required().min(3),
+    name: Joi.string().required().min(3),
   });
   const { error } = schema.validate(validatedItemBody);
   if (error) return res.status(400).send(error.details[0].message);
 };
 
-router.get("/:id", (req, res) => {
-  const item = validate404(req.params.id, res);
-  res.send(item.genre);
+router.get("/", async (req, res) => {
+  const genres = await Genre.find().sort("name");
+  res.send(genres);
 });
 
-router.post("/", (req, res) => {
-  validate400(req.body, res);
-  const genre = { id: genres.length + 1, genre: req.body.genre };
-  genres.push(genre);
+router.get("/:id", (req, res) => {
+  const genre = Genre.findById(req.params.id);
+  if (!genre) return res.status(404).send(`${req.body.name} is not found`);
   res.send(genre);
 });
 
-router.put("/:id", (req, res) => {
-  const item = validate404(req.params.id, res);
+router.post("/", async (req, res) => {
   validate400(req.body, res);
-  item.genre = req.body.genre;
-  res.send(item);
+  let genre = new Genre({
+    name: req.body.name,
+  });
+  try {
+    genre = await genre.save();
+    res.send(genre);
+    // await course.validate();
+  } catch (ex) {
+    for (const field in ex.errors) {
+      res.status(400).send(ex.errors[field].message);
+    }
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  validate400(req.body, res);
+  try {
+    const genre = await Genre.findByIdAndUpdate(
+      req.params.id,
+      { name: req.body.name },
+      { new: true }
+    );
+    if (!genre) return res.status(404).send(`${req.body.name} is not found`);
+    res.send(genre);
+  } catch (ex) {
+    for (const field in ex.errors) {
+      res.status(400).send(ex.errors[field].message);
+    }
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const genre = await Genre.findByIdAndRemove(req.params.id);
+  if (!genre) return res.status(404).send(`${req.body.name} is not found`);
+  res.send(genre);
 });
 
 module.exports = router;

306831df211657f0575d58f7d906bcaac1a7f9c6
Ibrahim Shaker <shaker.personnel@gmail.com>
2020-09-19 23:43:35 +0200

    welcome message

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("index", { title: "My Express App", message: "Welcome" });
+  res.render("index", {
+    title: "My Express App",
+    m1: "Express on Heroku",
+    m2:
+      "Welcome to my Express app on Heroku which is connected to Atlas MongoDB.",
+  });
 });
 
 module.exports = router;
